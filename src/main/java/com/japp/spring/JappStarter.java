0db412bdecfeee1517987f04e8e160508074ce32
Yeswanth Raghav T <tyraghav@outlook.com>
2019-09-22 15:53:50 +0530

    Basic Spring Application
    Initial Spring Application

---
+++ src/main/java/com/japp/spring/JappStarter.java
@@ -0,0 +1,13 @@
+package com.japp.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JappStarter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JappStarter.class, args);		
+	}
+
+}
